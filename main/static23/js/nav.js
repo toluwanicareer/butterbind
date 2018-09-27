c00209991ba9f4b57e5b4b9bd434ca8eb4b14454
tolu <abiodun.toluwanii@gmail.com>
2018-09-27 08:25:14 +0000

    fgfg

---
+++ main/static23/js/nav.js
@@ -0,0 +1,64 @@
+$('.navbar-toggle').click(function(){
+    $(this).toggleClass('collapsed');
+})
+
+function load_screen(){
+     $('#home').css('display', 'none');
+     $('footer').css('display', 'none');
+     $('.loading-container').css('display', 'block');
+   $('.line-1').css('display','block');
+    setTimeout(function (){$('.line-1').css('color','#4E6B0B')}, 2500);
+    setTimeout(function(){$('.line-2').removeClass('hidden-md')}, 3000);
+    setTimeout(function(){$('.line-2').css('display', 'block')}, 3000);
+    setTimeout(function(){$('.line-2').css('color','#4E6B0B')}, 5500);
+    setTimeout(function(){$('.line-3').removeClass('hidden-md')}, 6000);
+    setTimeout(function(){$('.line-3').css('display', 'block')}, 6000);
+    setTimeout(function(){$('.line-3').css('color','#4E6B0B')}, 8500);
+    setTimeout(function(){$('.line-5').removeClass('hidden-md')}, 9000);
+    setTimeout(function(){$('.line-5').css('display','block')}, 9000);
+    setTimeout(function(){$('.line-5').css('color','#4E6B0B')}, 11500);
+    setTimeout(function(){$('#quote_page').submit()}, 11500);
+
+}
+
+
+$('#quotebutton').click(function (e) {
+				e.preventDefault();
+				address=$('#address_input').val();
+				console.log(address)
+				if ( ! address){
+				    $('.hero-error-label').css('display', 'block');
+				    $('.hero-error-label').text('please enter address')
+
+				    return false;
+                }
+
+
+				$.post('/', {'address':address}).done(function (data) {
+					if (data.status){
+					    $('#addresskey').val(data.addresskey);
+					    $('#addressname').val(data.addressname);
+					    $('#quote_id').val(data.quote_id)
+					    load_screen()
+
+					    //alert('Data premium Price is: ' +data.annual_premium)
+
+					}
+					else{
+
+					    //alert(data.error);
+                        show_error_modal();
+
+
+					}
+                })
+            });
+
+
+
+function show_error_modal(){
+    console.log('im here ok');
+    $('#errorModalTitle').text('ERROR');
+    $('#errorModalBody').text('We ran into a problem pulling up property data information for this address. Please contact customersupport@swyfft.com.');
+    $('#errorModal').modal('show');
+}
---
+++ static/images/shield-logo-45x50-white.svg.bat
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 21.0.2, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 45 50.2" style="enable-background:new 0 0 45 50.2;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#FFFFFF;}
+</style>
+<path class="st0" d="M25.7,0h-6.3H0v10.7v17.8h0c0.4,12.1,10.3,21.8,22.5,21.8S44.6,40.5,45,28.4h0V10.7V0H25.7z M5.3,5.3H2.6V2.6
+	h2.6V5.3z M23.8,2.6h2.6v2.6h-2.6V2.6z M18.5,2.6h2.6v2.6h-2.6V2.6z M10.6,5.3H7.9V2.6h2.6V5.3z M13.2,2.6h2.6v2.6h-2.6V2.6z
+	 M31.4,19.8c-0.2,0-0.3,0-0.4,0c-1.1,0-1.9,0.4-2.5,1.1c-0.6,0.7-0.8,1.7-0.8,2.9l0,6.2c0,2.1-0.4,4-1.1,5.5
+	c-0.9,1.9-2.3,3.5-4.2,4.7c-1.9,1.2-3.9,1.8-5.9,1.8l-0.9,0v-8.8c0.2,0,0.3,0,0.4,0c1.1,0,1.9-0.4,2.5-1.1c0.6-0.7,0.8-1.7,0.8-2.9
+	l0-6.2c0-2.1,0.4-4,1.1-5.5c0.9-1.9,2.3-3.5,4.2-4.7c1.9-1.2,3.9-1.8,5.9-1.8l0.9,0V19.8z M31.8,5.3h-2.6V2.6h2.6V5.3z M37.1,5.3
+	h-2.6V2.6h2.6V5.3z M42.4,5.3h-2.6V2.6h2.6V5.3z"/>
+</svg>
