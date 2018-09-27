0704dccb48f633a0b54ca832063412bcf68c31f3
tolu <abiodun.toluwanii@gmail.com>
2018-09-27 06:02:47 +0000

    fgfg

---
+++ static/main.css.bat
@@ -0,0 +1,182 @@
+.line-md {
+    margin-bottom: 30px;
+}
+
+p, .line-1, .line-2, .line-3, .line-4, .line-5 {
+    transition: opacity 700ms;
+     font-size: 42px;
+     color:#fff;
+}
+
+.line-md{
+	display: none;
+}
+
+
+
+#quotebutton{
+    background-color: #8FBF21;
+    color: #FAFBFC;
+    border: none;
+    border-radius: 30px;
+    height: 56px;
+    width: auto;
+    padding: 11px 30px;
+    font-size: 14px;
+    font-weight: 300;
+    font-family: "Lato", sans-serif;
+    letter-spacing: 3px;
+    text-transform: uppercase;
+    transition: all 500ms ease-out 50ms;
+}
+#address_input {
+    font-family: "Lato", sans-serif;
+    font-weight: 300 !important;
+    letter-spacing: 0.4px;
+    background-color: #FAFBFC;
+    color: #000000 !important;
+    border-radius: 55px;
+    font-size: 14px !important;
+    font-style: italic;
+    height: 56px;
+}
+
+
+body {
+	padding-top:0px;
+}
+
+.address-input-container {
+    text-align: left;
+    margin-top: 110px;
+}
+
+* {
+    font-family: "Lato", sans-serif;
+    font-weight: 900;
+}
+
+.navbar-toggle .icon-bar {
+  width: 22px;
+  transition: all 0.2s;
+}
+.navbar-toggle .top-bar {
+  transform: rotate(45deg);
+  transform-origin: 10% 10%;
+}
+.navbar-toggle .middle-bar {
+  opacity: 0;
+}
+.navbar-toggle .bottom-bar {
+  transform: rotate(-45deg);
+  transform-origin: 10% 90%;
+}
+.navbar-toggle.collapsed .top-bar {
+  transform: rotate(0);
+}
+.navbar-toggle.collapsed .middle-bar {
+  opacity: 1;
+}
+.navbar-toggle.collapsed .bottom-bar {
+  transform: rotate(0);
+}
+
+
+.navbar-inverse {
+    background-color: transparent !important;
+    border-color: transparent !important;
+}
+
+.navbar-nav{
+
+    height:100vh;
+    background-color: #000;
+    margin-top:0px !important;
+}
+.navbar-collapse{
+    position:relative;
+    top:-53px;
+    max-height: inherit !important;
+}
+.navbar-toggle .icon-bar {
+    position: relative;
+    z-index: 3000;
+}
+
+.navbar
+
+.navbar .nav li a {
+    display: inline-block;
+    vertical-align: top;
+    padding: 0;
+    color: #cfcece;
+    font-size: 16px;
+    letter-spacing: 4px;
+    font-weight: 400;
+    text-transform: uppercase;
+}
+
+@media (max-width:768px){
+    .navbar .nav li a{
+        font-size: 13px;
+    }
+}
+.nav-elements-layout-cp {
+    margin: 0 0 4vh !important;
+}
+.header-first-line-md {
+    font-family: "Lato", sans-serif !important;
+    font-weight: 900;
+    font-style: normal;
+    font-size: 30px;
+    font-size: 4.5vh;
+    letter-spacing: 0.4px;
+    line-height: 1;
+    text-rendering: optimizeLegibility;
+    color: #4E6B0B;
+    margin-bottom: 0;
+    line-height: 1;
+    text-align: left;
+    margin-left: 20px;
+    margin-bottom: 20px;
+}
+
+@media (min-width: 1200px) {
+    .header-third-line-md {
+        font-size: 42px;
+    }
+}
+.header-third-line-md {
+    font-family: "Lato", sans-serif !important;
+    font-weight: 900;
+    font-style: normal;
+    font-size: 30px;
+    font-size: 4.5vh;
+    letter-spacing: 0.4px;
+    line-height: 1;
+    text-rendering: optimizeLegibility;
+    color: #FAFBFC;
+    margin-top: 0;
+    margin-bottom: 20px;
+    line-height: 1;
+}
+
+.home-message-container-bs {
+    background-color: #273803;
+    position: relative;
+    top:0px;
+    min-height: 100%;
+}
+
+.navbar-inverse {
+    background-color: #000;
+    border-color: #000;
+}
+
+.header-third-line-md {
+	margin-top: 20px;
+	}
+
+.line-1{
+    margin-top: 100px;
+}
