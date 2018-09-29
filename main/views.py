8ef5d616d1a1549a813a93b6936f13c6c327141d
Toluwani <abiodun.toluwanii@gmail.com>
2018-09-28 21:18:15 -0700

    'dfdf'

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 from .core import get_annual_premium, scrape
 import pdb
@@ -41,22 +42,8 @@ class Quote(TemplateView):
         url='https://www.swyfft.com/api/quotes/quoteid/'+quote_id
         response=requests.get(url)
         response=response.json()
-        deductibles=response['Elements']
-        primary_element=response['PrimaryElementName']
-        replacement_price=response['ReplacementCost']
-        for group in deductibles:
-            if group['ElementName']==primary_element:
-                deductibles_value=group['Value']
-                deductibles_choices=group['Choices']
-            if group['ElementName']=='ReplacementCost':
-                replacement_price=group['Value']
-        return render(request, 'quote.html', {'annual_premium':(response['GrossAnnualPayment']),
-                                       'address':address,
-                                        'quote_id':quote_id,
-                                       'deductibles_value':deductibles_value,
-                                       'deductibles_choices':deductibles_choices,
-                                        'primaryElementName':primary_element,
-                                       'replacement_price':replacement_price})
+        data=render_to_include(response,address,quote_id)
+        return render(request, 'quote.html', data)
 
 class Privacy(TemplateView):
     template_name = 'privacy.html'
@@ -65,15 +52,45 @@ class Privacy(TemplateView):
 class PatchQuote(View):
 
     def get(self,request, *args, **kwargs):
-        elementname=request.GET.get('elementname')
-        quote_id=request.GET.get('quote_id')
+        elementname=request.GET.get('ElementName')
+        quote_id=request.GET.get('quoteid')
+        address=request.GET.get('address')
         value=request.GET.get('value')
         addresskey=request.GET.get('addresskey')
         url='https://www.swyfft.com/api/quotes'
-        Elements=[{'ElementName':elementname, 'Value':value}]
-        payload={'ConfiguredOn':'null', 'Elements':Elements, 'ManualPremiumAdjustment':0, 'QuoteId':quote_id,
+        Elements=[{"ElementName":elementname, "Value":float(value)}]
+        payload={'ConfiguredOn':None, 'Elements':Elements, 'ManualPremiumAdjustment':0, 'QuoteId':quote_id,
                  'gaEventAction':''}
-        response=requests.patch(url,data
 
-        =payload)
-        pdb.set_trace()
+        response=requests.patch(url, json=payload)
+        response=response.json()
+        data=render_to_include(response,address, quote_id)
+        string=render_to_string('includes/quote_section.html', context=data)
+        return JsonResponse({'status':200, 'data':string})
+
+
+def render_to_include(response, address,quote_id ):
+    deductibles = response['Elements']
+    #pdb.set_trace()
+    primary_element = response['PrimaryElementName']
+    replacement_price = response['ReplacementCost']
+    for group in deductibles:
+        if group['ElementName'] == primary_element:
+            deductibles_value = group['Value']
+            deductibles_choices = group['Choices']
+        if group['ElementName'] == 'ReplacementCost':
+            replacement_price = group['Value']
+            min_value=group['MinValue']
+            max_value=group['MaxValue']
+    data={'annual_premium': (response['GrossAnnualPayment']),
+                                          'address': address,
+                                          'quote_id': quote_id,
+                                          'deductibles_value': deductibles_value,
+                                          'deductibles_choices': deductibles_choices,
+                                          'primaryElementName': primary_element,
+                                          'replacement_price': replacement_price,
+                                            'min_value': min_value,
+                                            'max_value': max_value
+
+          }
+    return data
